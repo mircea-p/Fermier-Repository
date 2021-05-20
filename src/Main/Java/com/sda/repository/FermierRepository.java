3b966c563b6f8a57608f82b03a18ebde67ad11c4
40751 <cristyardy2@gmail.com>
2021-05-20 21:03:41 +0300

    Ultima actualizare a codului.

--- src/Main/Java/com/sda/repository/FermierRepository.java
+++ src/Main/Java/com/sda/repository/FermierRepository.java
@@ -1,13 +1,8 @@
 package com.sda.repository;
 
-import com.sda.model.Fermier;
 import com.sda.utils.SessionManager;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 public class FermierRepository {
 
--- src/Main/Java/com/sda/utils/SessionManager.java
+++ src/Main/Java/com/sda/utils/SessionManager.java
@@ -1,12 +1,7 @@
 package com.sda.utils;
 
-import com.sda.model.Fermier;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
-import org.hibernate.service.ServiceRegistry;
-
 
 
 public class SessionManager extends AbstractSessionManager
