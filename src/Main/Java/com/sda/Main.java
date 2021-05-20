c6d2ce36ff71260b450743bfd5f039a1be405f05
Cristi <gigicristian.momoiu@gmail.com>
2021-05-20 20:12:41 +0300

    Am adaugat clasa de fermier!

--- src/Main/Java/com/sda/Main.java
+++ src/Main/Java/com/sda/Main.java
@@ -1,4 +1,19 @@
 package com.sda;
 
+import com.sda.model.Fermier;
+import com.sda.repository.FermierRepository;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Fermier fermier1 = new Fermier("AGRO", 123456, "agro@agro.ro");
+        Fermier fermier2 = new Fermier("AgroJonny", 524682, "agrojonny@agro.com");
+
+        FermierRepository fermierRepository = new FermierRepository();
+        fermierRepository.save(fermier1);
+        fermierRepository.save(fermier2);
+
+        
+    }
 }
--- src/Main/Java/com/sda/model/Fermier.java
+++ src/Main/Java/com/sda/model/Fermier.java
@@ -1,4 +1,74 @@
 package com.sda.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "fermieri")
 public class Fermier {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "fermier_id")
+    private Integer fermierId;
+
+    @Column(name = "nume_fermier")
+    private String numeFermier;
+
+    @Column(name = "cui")
+    private Integer cui;
+
+    @Column(name = "email")
+    private String email;
+
+
+    public Fermier() {
+    }
+
+    public Fermier(String numeFermier, Integer cui, String email) {
+        this.numeFermier = numeFermier;
+        this.cui = cui;
+        this.email = email;
+    }
+
+    public Integer getFermierId() {
+        return fermierId;
+    }
+
+    public void setFermierId(Integer fermierId) {
+        this.fermierId = fermierId;
+    }
+
+    public String getNumeFermier() {
+        return numeFermier;
+    }
+
+    public void setNumeFermier(String numeFermier) {
+        this.numeFermier = numeFermier;
+    }
+
+    public Integer getCui() {
+        return cui;
+    }
+
+    public void setCui(Integer cui) {
+        this.cui = cui;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Fermier{" +
+                "fermierId=" + fermierId +
+                ", numeFermier='" + numeFermier + '\'' +
+                ", cui=" + cui +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
